1f0ccad69f03fa5ba235f02c7a06dde8ede9e77a
Mahadi Hasan <69779118+Mahadibappi@users.noreply.github.com>
2022-10-27 07:44:44 +0600

    form added

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -5,22 +5,38 @@ function SignUp() {
   return (
     <Form>
       <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Your Name</Form.Label>
+        <Form.Control
+          className="w-50"
+          name="name"
+          type="text"
+          placeholder="Enter Name"
+        />
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
+        <Form.Control
+          className="w-50"
+          name="email"
+          type="email"
+          placeholder="Enter email"
+        />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control
+          className="w-50"
+          name="password"
+          type="password"
+          placeholder="Password"
+        />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
+        <Form.Text className="text-muted">
+          We'll never share your email with anyone else.
+        </Form.Text>
       </Form.Group>
       <Button variant="primary" type="submit">
-        Submit
+        SignUP
       </Button>
     </Form>
   );
